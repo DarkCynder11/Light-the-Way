2ad2663dd6ddb0037bc2f3cb166e896a57d7369d
Mya Cordner-Cassidy <m.j.cordnercassidy@gmail.com>
2023-01-31 12:23:17 -0500

    Added a collision detection for light and interactable objects

---
+++ Light the Way/Assets/Scripts/LightDetetion.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDetetion : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Blue")
+        {
+            print("shows blue object");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Blue")
+        {
+            print("interactable");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Blue")
+        {
+            print("changes back to gray");
+        }
+    }
+}
