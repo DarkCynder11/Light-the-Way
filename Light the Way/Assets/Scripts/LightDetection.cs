e5f0adfef8b864c9a81008384ad228df4315911c
Mya Cordner-Cassidy <m.j.cordnercassidy@gmail.com>
2023-02-28 13:42:15 -0500

    got colour detetion between light and object tag working

--- Light the Way/Assets/Scripts/LightDetection.cs
+++ Light the Way/Assets/Scripts/LightDetection.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class LightDetection : MonoBehaviour
 {
+    public Light colourLight;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Blue")
+        if (other.gameObject.tag == "Blue" && colourLight.color == Color.blue)
         {
             print("shows blue object");
+        }
+        else if (other.gameObject.tag == "Green" && colourLight.color == Color.green)
+        {
+            print("shows green object");
         }
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Blue")
+        if (other.gameObject.tag == "Blue" && colourLight.color == Color.blue)
+        {
+            print("interactable");
+        }
+        else if (other.gameObject.tag == "Green" && colourLight.color == Color.green)
         {
             print("interactable");
         }
@@ -23,9 +32,14 @@ public class LightDetection : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Blue")
+        if (other.gameObject.tag == "Blue" && colourLight.color == Color.blue)
         {
             print("changes back to gray");
         }
+        else if (other.gameObject.tag == "Green" && colourLight.color == Color.green)
+        {
+            print("changes back to gray");
+        }
+
     }
 }
--- Light the Way/Assets/Scripts/LightInteraction.cs
+++ Light the Way/Assets/Scripts/LightInteraction.cs
@@ -5,31 +5,88 @@ using UnityEngine;
 public class LightInteraction : MonoBehaviour
 {
     public Light lightToCheck; // the light asset to check the color of
-    public GameObject interactableObject; // the tagged object to make interactable
+    public string requiredColorName; // the name of the required color for the light asset
+    public string interactableObjectTag; // the tag of the interactable object
     public string shaderName; // the name of the shader to apply
     public Color shaderColor; // the color to apply to the shader
 
-    private Renderer interactableRenderer; // the renderer of the interactable object
+    private Renderer interactableRenderer; // the renderer of the current interactable object
 
     void Start()
     {
-        interactableRenderer = interactableObject.GetComponent<Renderer>();
+        interactableRenderer = null;
     }
 
     void Update()
     {
         // Check if the light color is the desired color
-        if (lightToCheck.color == Color.green)
+        if (lightToCheck != null && lightToCheck.color == GetRequiredColor())
         {
-            // Make the interactable object interactable
-            interactableObject.GetComponent<Collider>().enabled = true;
+            // Make sure the interactable object is still in the light
+            if (interactableRenderer == null || !interactableRenderer.isVisible)
+            {
+                interactableRenderer = null;
 
-            // Change the shader of the interactable object
-            interactableRenderer.material.shader = Shader.Find(shaderName);
-            interactableRenderer.material.SetColor("_SpecColor", shaderColor);
+                // Find the interactable object in the light
+                Collider[] hitColliders = Physics.OverlapBox(transform.position, transform.localScale / 2f);
+                foreach (Collider hitCollider in hitColliders)
+                {
+                    if (hitCollider.CompareTag(interactableObjectTag))
+                    {
+                        interactableRenderer = hitCollider.GetComponent<Renderer>();
+                        break;
+                    }
+                }
+            }
+
+            // If an interactable object is in the light, make it interactable and change its shader
+            if (interactableRenderer != null)
+            {
+                interactableRenderer.GetComponent<Collider>().enabled = true;
+                interactableRenderer.material.shader = Shader.Find(shaderName);
+                interactableRenderer.material.SetColor("_SpecColor", shaderColor);
+            }
+        }
+        else
+        {
+            // If an interactable object was being affected by the light, make it non-interactable and reset its shader
+            if (interactableRenderer != null)
+            {
+                interactableRenderer.GetComponent<Collider>().enabled = false;
+                interactableRenderer.material.shader = Shader.Find("Standard");
+                interactableRenderer.material.SetColor("_SpecColor", Color.black);
+                interactableRenderer = null;
+            }
+        }
+    }
+
+    private Color GetRequiredColor()
+    {
+        // Convert the name of the required color to a Color object
+        switch (requiredColorName.ToLower())
+        {
+            case "red":
+                return Color.red;
+            case "green":
+                return Color.green;
+            case "blue":
+                return Color.blue;
+            case "yellow":
+                return Color.yellow;
+            case "cyan":
+                return Color.cyan;
+            case "magenta":
+                return Color.magenta;
+            case "white":
+                return Color.white;
+            case "black":
+                return Color.black;
+            default:
+                return Color.clear;
         }
     }
 }
+
 
 
 
