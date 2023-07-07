d636b105e33e64a5202fb26be17f5652a0bc3a6a
Mya Cordner-Cassidy <m.j.cordnercassidy@gmail.com>
2023-07-07 11:51:24 -0400

    collectible test

---
+++ Light the Way/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public int scoreValue = 1; // The value of the collectible (e.g., score points)
+    public AudioClip collectSound; // Sound effect to play when the collectible is collected
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+
+    private void Collect()
+    {
+        // Update the player's score
+        GameManager.Instance.AddScore(scoreValue);
+
+        // Play a sound effect
+        AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+        // Remove the collectible from the scene
+        Destroy(gameObject);
+    }
+}
+
--- Light the Way/Assets/Scripts/GameManager.cs
+++ Light the Way/Assets/Scripts/GameManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    private int score;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int points)
     {
-        
+        score += points;
+        Debug.Log("Score: " + score);
     }
 }
+
