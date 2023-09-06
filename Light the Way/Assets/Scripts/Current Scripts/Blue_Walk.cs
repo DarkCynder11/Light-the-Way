4e264d57d636800d7cf1169f6a6dda336bd360e6
Mya Cordner-Cassidy <m.j.cordnercassidy@gmail.com>
2023-09-06 11:43:56 -0400

    started making changes to code

---
+++ Light the Way/Assets/Scripts/Current Scripts/Blue_Walk.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blue_Walk : MonoBehaviour, IInteractable
+{
+
+    public void Interact()
+    {
+        Debug.Log("water was interacted with");
+    }
+}
